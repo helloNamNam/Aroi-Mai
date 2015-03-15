cd679c2b34f53a2049a5ba8d1cf20b215abc9be9
olarMiEzZlo <s7070025@kmitl.ac.th>
2015-03-15 18:11:44 +0700

    Create game function
    Edit main function and add game function to play a game (can't run it
    now cuz wait two function board and event) :)

--- code_project.c
+++ code_project.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-int board(char, int); /* Function print game's board to play */
+#include <string.h>
+int board(int[*]); /* Function print board to play game */
+int game(char, int); /* Function change turn and rolls die to send to function board */
+int event(int[*]); /* Function random event when player move on event square grid */
 int main() /* Get mode to play or read the rules */
 {
 	char mode[1], confirm[1];
@@ -19,7 +22,7 @@ int main() /* Get mode to play or read the rules */
 		{
 			printf("\n=== Rules ===\n");
 			printf("1) All players start on square grid 1 and have\n   three hearts if the player's hurt all\n   the player will lose.\n");
-			printf("2) Takes turns to roll a single die to move \n   by the number of squares indicated by \n   the die roll.\n");
+			printf("2) Takes turns to roll a single dice to move \n   by the number of squares indicated by \n   the dice roll.\n");
 			printf("3) If, on completion of a move, a player is\n   lands on event grid <\?\?\?>, the player\n   must move up or down follow the event.\n");
 			printf("4) If a player rolls a 6, the player take\n   another turn; otherwise play passes to the\n   next player in turn.\n");
 			printf("5) The player who is first go to the last\n   square is the winner.\n");
@@ -72,10 +75,84 @@ int main() /* Get mode to play or read the rules */
 			else
 				printf("Player B is first player.\n");
 		}
-		board(mode, fp);
+		game(mode, fp);
 	}
 }
-int board(char mode[1], int fp)
+int game(char mode[1], int fp)
 {
-	
+	char dice[4];
+	int table[90] = {0};
+	int tp = 0, tb = 1, point[2] = {0}, lp[2] = {4}, walk, e_type;
+	if (mode[0] == '1') /* Single player */
+	{
+		if (fp) /* player is first */
+		{
+			tp = 1;
+			tb = 0;
+		}
+		board(point);
+		while (1)
+		{
+			if (tp) /* Player's turn */
+			{
+				printf("It's your turn please type 'Dice' to roll the dice\nlife point (");
+				for (i = 1; i < 4; ++i)
+				{
+					if (lp[0] >= i)
+						printf("*");
+					else
+						printf("-");
+				}
+				printf(") : ");
+				scanf("%s", dice);
+				while (strcmp(dice, "Dice") != 0)
+				{
+					printf("Incorrect! Please type 'Dice' to roll the dice : ");
+				}
+				walk = rand() % 6 + 1;
+				printf("Result : %d\n", walk);
+				point[0] += walk;
+				board(point);
+				while (table[point[0]]) /* Have an event */
+				{
+					e_type = rand() % 10;
+					if ()
+						point[0] = event(e_type);
+					else
+						lp[0] = event(e_type);
+					if (point[0] >= 89 || lp[0] == 0) /* If have win or lose player after event */
+						break;
+				}
+				if (walk != 6) /* Change turn */
+				{
+					tp = 0;
+					tb = 1;
+				}
+			}
+			else /* Bot's turn */
+			{
+				printf("It's Bot turn to roll the dice\nlife point (");
+				for (i = 1; i < 4; ++i)
+				{
+					if (lp[1] >= i)
+						printf("*");
+					else
+						printf("-");
+				}
+				printf(")");
+				walk = rand() % 6 + 1;
+				printf("Result : %d\n", walk);
+				point[1] += walk;
+				board(point);
+				if (walk != 6) /* Change turn */
+				{
+					tp = 1;
+					tb = 0;
+				}
+			}
+		}
+	}
+	else /* Two players */
+	{
+	}
 }
